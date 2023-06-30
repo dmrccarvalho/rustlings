9e4c17c0a6c1235dcc86a82369e482f8b5233fd2
Diogo Carvalho <dmrccarvalho@gmail.com>
2023-06-30 14:50:16 +0100

    Update errors2.rs

--- exercises/error_handling/errors2.rs
+++ exercises/error_handling/errors2.rs
@@ -19,15 +19,23 @@
 // Execute `rustlings hint errors2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// I AM N0T DONE
 
 use std::num::ParseIntError;
 
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
-    let qty = item_quantity.parse::<i32>();
+    let qty = item_quantity.parse::<i32>()?; // Instead of having the ?, see the comment below
 
+    // Take into account the following pattern:
+    /*match qty {
+        Ok(value) => Ok(value * cost_per_item + processing_fee),
+        Err(error) => Err(error)
+    }*/
+    // In this pattern we have Err(something) => Err(something), which is redundant.
+    // Instead we can use the ? operator to directly propagate any errors or unwrap the value in case of success.
+    
     Ok(qty * cost_per_item + processing_fee)
 }
 
