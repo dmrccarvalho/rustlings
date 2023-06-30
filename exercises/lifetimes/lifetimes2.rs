ae37e172c82805be15f4ea7158e1a1ab438abb02
Diogo Carvalho <dmrccarvalho@gmail.com>
2023-06-30 18:02:14 +0100

    Update lifetimes2.rs

--- exercises/lifetimes/lifetimes2.rs
+++ exercises/lifetimes/lifetimes2.rs
@@ -6,7 +6,7 @@
 // Execute `rustlings hint lifetimes2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// I AM N0T DONE
 
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
@@ -20,8 +20,8 @@ fn main() {
     let string1 = String::from("long string is long");
     let result;
     {
-        let string2 = String::from("xyz");
+        let string2 = String::from("xyz");  // Alternatively, this can go outside this code block (see below)
         result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is '{}'", result); // Moved here (previously was before code block)
     }
-    println!("The longest string is '{}'", result);
 }
